c3222ceabc7b6d1c02ed34e774321612c05fe1f4
jamil2gomes <jamil.lannister23@gmail.com>
2020-02-10 09:46:13 -0300

    remodelagem do produto

--- projeto_estoque/lib/modelo/produto.dart
+++ projeto_estoque/lib/modelo/produto.dart
@@ -1,55 +1,53 @@
 class Produto {
-  int _id;
-  String _nome;
-  String _descricao;
-  String _categoria;
-  int _quantidade;
-  bool _prioridade;
+  int id;
+  String nome;
+  String descricao;
+  String categoria;
+  int quantidade;
+  bool prioridade;
 
   Produto(
-      {int id,
-      String nome,
-      String categoria,
-      int quantidade,
-      bool prioridade,
-      String descricao}) {
-    this._id = id;
-    this._nome = nome;
-    this._descricao = descricao;
-    this._categoria = categoria;
-    this._quantidade = quantidade;
-    this._prioridade = prioridade;
-  }
+      {this.id,
+      this.nome,
+      this.categoria,
+      this.quantidade,
+      this.prioridade,
+      this.descricao});
 
-  int get id => _id;
-  set id(int id) => _id = id;
-  String get nome => _nome;
-  set nome(String nome) => _nome = nome;
-  String get descricao => _descricao;
-  set descricao(String descricao) => _descricao = descricao;
-  String get categoria => _categoria;
-  set categoria(String categoria) => _categoria = categoria;
-  int get quantidade => _quantidade;
-  set quantidade(int quantidade) => _quantidade = quantidade;
-  bool get prioridade => _prioridade;
-  set prioridade(bool prioridade) => _prioridade = prioridade;
+  // int get id => _id;
+  // set id(int id) => _id = id;
+  // String get nome => _nome;
+  // set nome(String nome) => _nome = nome;
+  // String get descricao => _descricao;
+  // set descricao(String descricao) => _descricao = descricao;
+  // String get categoria => _categoria;
+  // set categoria(String categoria) => _categoria = categoria;
+  // int get quantidade => _quantidade;
+  // set quantidade(int quantidade) => _quantidade = quantidade;
+  // bool get prioridade => _prioridade;
+  // set prioridade(bool prioridade) => _prioridade = prioridade;
 
   Produto.fromJson(Map<String, dynamic> json) {
-    _id = json['id'];
-    _nome = json['nome'];
-    _descricao = json['descricao'];
-    _categoria = json['categoria'];
-    _quantidade = json['quantidade'];
+    id = json['id'];
+    nome = json['nome'];
+    descricao = json['descricao'];
+    categoria = json['categoria'];
+    quantidade = json['quantidade'];
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['id'] = this._id;
-    data['nome'] = this._nome;
-    data['descricao'] = this._descricao;
-    data['categoria'] = this._categoria;
-    data['quantidade'] = this._quantidade;
+    data['id'] = this.id;
+    data['nome'] = this.nome;
+    data['descricao'] = this.descricao;
+    data['categoria'] = this.categoria;
+    data['quantidade'] = this.quantidade;
 
     return data;
+  }
+
+  @override
+  String toString() {
+    return "Produto[$nome, $descricao]";
   }
 }
